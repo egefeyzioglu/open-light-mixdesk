b544b5a57f5ccdc0f3189903f4be00ef0616d46a
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2020-05-03 06:29:29 +0300

    Added support to save configurations

--- src/Constants.java
+++ src/Constants.java
@@ -1,22 +1,50 @@
+import java.lang.reflect.Field;
+
 public class Constants {
-	public static final int SERIAL_COMM_BAUD_RATE = 2000000;
-	public static final String SERIAL_COMM_PORT_NAME = "COM1";
+	public static int SERIAL_COMM_BAUD_RATE = 2000000;
+	public static String SERIAL_COMM_PORT_NAME = "COM1";
 	/**
 	 * The number of times per second the DMX data is transmitted to the fixtures
 	 */
-	public static final int DMX_DATA_FREQUENCY = 20;
-	public static final long DMX_DATA_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) DMX_DATA_FREQUENCY));
-	public static final int DMX_CHANNEL_COUNT = 512;
+	public static int DMX_DATA_FREQUENCY = 20;
+	public static long DMX_DATA_PERIOD_NANOS;
+	public static int DMX_CHANNEL_COUNT = 512;
 	
-	public static final int FIXTURE_UPDATE_FREQUENCY = 20;
-	public static final long FIXTURE_UPDATE_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) DMX_DATA_FREQUENCY));
+	public static int FIXTURE_UPDATE_FREQUENCY = 20;
+	public static long FIXTURE_UPDATE_PERIOD_NANOS;
 	
-	public static final int FADER_UPDATE_FREQUENCY = 20;
-	public static final long FADER_UPDATE_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) FADER_UPDATE_FREQUENCY));
+	public static int FADER_UPDATE_FREQUENCY = 20;
+	public static long FADER_UPDATE_PERIOD_NANOS;
 	
-	public static final int MAIN_LOOP_FREQUENCY = 100;
-	public static final long MAIN_LOOP_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) MAIN_LOOP_FREQUENCY));
+	public static int MAIN_LOOP_FREQUENCY = 100;
+	public static long MAIN_LOOP_PERIOD_NANOS;
 	
-	public static final int FADER_COUNT = 12;
-	public static final int FIXTURE_COUNT = 12;
+	public static int FADER_COUNT = 12;
+	public static int FIXTURE_COUNT = 12;
+	
+	public static String PREFERENCES_PATH;
+	
+	public static void init() {
+		DMX_DATA_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) DMX_DATA_FREQUENCY));
+		FIXTURE_UPDATE_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) DMX_DATA_FREQUENCY));
+		FADER_UPDATE_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) FADER_UPDATE_FREQUENCY));
+		MAIN_LOOP_PERIOD_NANOS = (long) (1000 * 1000 * 1000 * ((double) 1 / (double) MAIN_LOOP_FREQUENCY));
+	}
+	
+	public static void printAll() {
+		System.out.print("Printing preferences\n\n");
+		
+		Field[] fields = Constants.class.getDeclaredFields();
+		for(Field field: fields) {
+			try {
+				System.out.println(field.getName() + ": " + field.get(null).toString());
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				//Ignore
+			} catch(NullPointerException e) {
+				System.out.println(field.getName() + ": null");
+			}
+		}
+	}
+	
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Profile.Section;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Main {
 	
 	/**
@@ -19,22 +32,43 @@ public class Main {
 	private static DMXWriter dmxWriter;
 	private static FixtureManager fixtureManager;
 	private static FaderManager faderManager;
+
+	private static FixtureTemplate[] fixtureTemplates;
 	
 	public static void main(String[] args) {
+		try {
+			setPreferences();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		dmxWriter = new DMXWriter();
+		
+		Ini ini = null;
+		try {
+			ini = new Ini(new File(Constants.PREFERENCES_PATH));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
 		dmxChannels = new int[Constants.DMX_CHANNEL_COUNT]; //Auto initialised to 0
 		faders = new Fader[Constants.FADER_COUNT];
-		setFaders();
 		fixtures = new Fixture[Constants.FIXTURE_COUNT];
-		setFixtures();
+		setFixtures(ini);
+		setFaders(ini);
 		
-		dmxWriter = new DMXWriter();
-		getDmxWriter().start();
-		
-		fixtureManager = new FixtureManager();
-		fixtureManager.start();
-		
-		faderManager = new FaderManager();
-		faderManager.start();
+		synchronized(dmxWriter) {
+			dmxWriter = new DMXWriter();
+			getDmxWriter().start();
+			
+			fixtureManager = new FixtureManager();
+			fixtureManager.start();
+			
+			faderManager = new FaderManager();
+			faderManager.start();
+		}
 		
 		while(!Thread.interrupted()) {
 			long start = System.nanoTime();
@@ -47,25 +81,171 @@ public class Main {
 		}
 	}
 
-	private static void setFixtures() {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Unwritten method stub!");
+	private static void setFixtures(Ini ini) {
+		JSONArray fixtureTemplatesJSON = null;
+		JSONArray fixturesJSON = null;
+		
+		try {
+			fixtureTemplatesJSON = (JSONArray) new JSONParser().parse(ini.get("fixture-templates").get("array"));
+			fixturesJSON = (JSONArray) new JSONParser().parse(ini.get("fixtures").get("array"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		int index = 0;
+		
+		for(Object fixtureTemplateJSONRaw: fixtureTemplatesJSON) {
+			
+			JSONObject fixtureTemplateJSON;
+			
+			if(fixtureTemplateJSONRaw instanceof JSONObject)
+				fixtureTemplateJSON = (JSONObject) fixtureTemplateJSONRaw;
+			else
+				throw new IllegalArgumentException("Fixture template not a valid JSON object!");
+
+			if(fixtureTemplates == null)
+				fixtureTemplates = new FixtureTemplate[1];
+			else
+				fixtureTemplates = Arrays.copyOf(fixtureTemplates, fixtureTemplates.length + 1);
+			
+			fixtureTemplates[Math.toIntExact((long) fixtureTemplateJSON.get("uid"))]
+					= new FixtureTemplate(
+										(String) fixtureTemplateJSON.get("name"),
+										Math.toIntExact((long) fixtureTemplateJSON.get("numChannels")),
+										jsonArrayToStringArray((JSONArray) fixtureTemplateJSON.get("channelDescriptions")),
+										Math.toIntExact((long) fixtureTemplateJSON.get("dimOffset"))
+					);
+			index++;
+		}
+		
+		index = 0;
+		
+		for(Object fixtureJSONRaw: fixturesJSON) {
+			
+			JSONObject fixtureJSON;
+			
+			if(fixtureJSONRaw instanceof JSONObject)
+				fixtureJSON = (JSONObject) fixtureJSONRaw;
+			else
+				throw new IllegalArgumentException("Fixture not a valid JSON object!");
+			
+			fixtures[index] = new Fixture(
+											fixtureTemplates[Math.toIntExact((long) fixtureJSON.get("templateUid"))],
+											Math.toIntExact((long) fixtureJSON.get("firstChannel")),
+											jsonArrayToIntArray((JSONArray) fixtureJSON.get("channelValues"))
+							);
+			
+			index++;
+		}
+		
 	}
 
-	private static void setFaders() {
-		// TODO Auto-generated method stub
-		throw new IllegalStateException("Unwritten method stub!");
+	private static String[] jsonArrayToStringArray(JSONArray in) {
+		int size = in.size();
+		String[] out = new String[size];
+		Object[] arr = in.toArray();
+		size--;
+		while(size > 0) {
+			out[size] = (String) arr[size--];
+		}
+		
+		return out;
+	}
+	
+	private static int[] jsonArrayToIntArray(JSONArray in) {
+		int size = in.size();
+		int[] out = new int[size];
+		Object[] arr = in.toArray();
+		size--;
+		while(size > 0) {
+			if(arr[size--] instanceof String)
+				out[size] = Integer.parseInt((String) arr[size--]);
+			else
+				out[size] = Math.toIntExact((long) arr[size--]);
+		}
+		
+		return out;
+	}
+
+	private static void setFaders(Ini ini) {
+		JSONArray fadersJson = null;
+		
+		try {
+			fadersJson = (JSONArray) new JSONParser().parse(ini.get("faders").get("array"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		int index = 0;
+		
+		for(Object faderJsonRaw: fadersJson.toArray()) {
+			JSONObject faderJson;
+			
+			if(faderJsonRaw instanceof JSONObject)
+				faderJson = (JSONObject) faderJsonRaw;
+			else
+				throw new IllegalArgumentException("Fader not a valid JSON object!");
+					
+			
+			
+			faders[index] = new Fader(
+								getFixtures(jsonArrayToIntArray((JSONArray) (faderJson.get("fixtureIds")))),
+								jsonArrayToIntArray((JSONArray) (faderJson.get("fixtureChannels"))),
+								jsonArrayToIntArray((JSONArray) (faderJson.get("fixtureValues"))),
+								Math.toIntExact((long) faderJson.get("value"))
+							);
+			
+			index++;
+		}
+		
 	}
 
 	public static void onError(ErrorType type, String message) {
 		//TODO: Implement this in the GUI
 		System.err.println(type.toString() + ": " + message);
 	}
+	
+	private static void setPreferences() throws InvalidFileFormatException, IOException{
+		InputStream is = Main.class.getClassLoader().getResource("preferencesPath.txt").openStream();
+		Constants.PREFERENCES_PATH = "";
+		while(is.available() > 0)
+			Constants.PREFERENCES_PATH += (char) is.read();
+		
+		Ini ini = new Ini(new File(Constants.PREFERENCES_PATH));
+		Section preferences = ini.get("preferences");
+		Section constants = ini.get("constants");
 
+		Constants.DMX_CHANNEL_COUNT = Integer.parseInt(preferences.get("DMX_CHANNEL_COUNT"));
+		Constants.FADER_COUNT = Integer.parseInt(preferences.get("FADER_COUNT"));
+		Constants.FIXTURE_COUNT = Integer.parseInt(preferences.get("FIXTURE_COUNT"));
+		Constants.PREFERENCES_PATH = preferences.get("PREFERENCES_PATH");
+		Constants.MAIN_LOOP_FREQUENCY = Integer.parseInt(preferences.get("MAIN_LOOP_FREQUENCY"));
+		Constants.FADER_UPDATE_FREQUENCY = Integer.parseInt(preferences.get("FADER_UPDATE_FREQUENCY"));
+		Constants.FIXTURE_UPDATE_FREQUENCY = Integer.parseInt(preferences.get("FIXTURE_UPDATE_FREQUENCY"));
+		Constants.DMX_DATA_FREQUENCY = Integer.parseInt(preferences.get("DMX_DATA_FREQUENCY"));
+		
+
+		Constants.SERIAL_COMM_BAUD_RATE = Integer.parseInt(constants.get("SERIAL_COMM_BAUD_RATE"));
+		Constants.SERIAL_COMM_PORT_NAME = constants.get("SERIAL_COMM_PORT_NAME");
+		
+		Constants.init();
+		Constants.printAll();
+	}
+	
 	/**
 	 * @return the {@link #dmxWriter}
 	 */
 	public static DMXWriter getDmxWriter() {
 		return dmxWriter;
+	}
+	
+	public static Fixture[] getFixtures(int[] indexes) {
+		Fixture[] out = new Fixture[indexes.length];
+		for(int i = 0; i < indexes.length; i++)
+			out[i] = fixtures[indexes[i]];
+		
+		return out;
 	}
 }
